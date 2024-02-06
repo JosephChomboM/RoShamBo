12f8aef5eb59a98f1f13a0b33f93afc74a692823
Joseph Chombo Morocho <josephalexander0704@gmail.com>
2024-02-06 01:01:06 -0500

    Incorporating best_of function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+from math import ceil
     
 def title():
     tex ='''
@@ -15,27 +15,49 @@ def title():
     return print(tex)
 
 title()
-options = ('rock', 'paper', 'scissors')
-user = input(f"What is your choice: {' '.join(options)}?\n").lower()
-pc = random.choice(options)
 
+options = ('rock', 'paper', 'scissors')
 def play():
+    user = None
+    while user not in options:
+        user = input(f"What is your choice: {' '.join(options)}?\n").lower()
+    print(type(options[0]))
+    pc = random.choice(options)
     if user == pc:
-        return "Both --> {}.\n\tIT'S A TIE ¯\_(ツ)_/¯".format(pc)
+        return (0, user, pc)
     if is_win(user, pc):
-        return "You --> {}\tVS\t{} <-- Pc\n\tYOU WON!".format(user, pc)
-    return "You --> {}\tVS\t{} <-- Pc\n\tYOU LOST :(".format(user, pc)
+        return (1, user, pc)
+    
+    return (-1, user, pc)
 
 
 def is_win(my, opponent):
-    if (
-        (my == options[0] and opponent == options[2]) or
-        (my == options[1] and opponent == options[0]) or
-        (my == options[2] and opponent == options[1])
-    ):
+    if ((my == options[0] and opponent == options[2]) or (my == options[2] and opponent == options[1]) or (my == options[1] and opponent == options[0])):
         return True
     return False
 
+def best_of(num):
+    my_wins = 0
+    pc_wins = 0
+    necessary_rounds = ceil(num / 2)
+    
+    while my_wins < necessary_rounds and pc_wins < necessary_rounds:
+        result, user, pc = play()
+        if result == 0:
+            print("Both --> {}.\n\tIT'S A TIE ¯\_(ツ)_/¯".format(pc))
+            
+        elif result == 1:
+            my_wins += 1
+            print("You --> {}\tVS\t{} <-- Pc\n\tYOU WON!".format(user, pc))
+        else:
+            pc_wins += 1
+            print("You --> {}\tVS\t{} <-- Pc\n\tYOU LOST :(".format(user, pc))
+    
+    if my_wins > pc_wins:
+        print('\nYou won the Best of {}.'.format(num))        
+    else:
+        print('The pc won the Best of {}.'.format(num))
+
 
 if __name__ == '__main__':
-    print(play())
+    best_of(3)
